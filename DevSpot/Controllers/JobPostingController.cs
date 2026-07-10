180783102eb82ee90df758308f3a0a99822f5473
Tulasisree <sree.mulagapaka@gmail.com>
2026-07-10 15:51:51 +0530

    Adding Authorization

--- DevSpot/Controllers/JobPostingController.cs
+++ DevSpot/Controllers/JobPostingController.cs
@@ -2,12 +2,13 @@ using System.Runtime.CompilerServices;
 using DevSpot.Models;
 using DevSpot.Respositories;
 using DevSpot.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Packaging.Core;
 
 namespace Controllers.JobPostingController;
-
+[Authorize]
 public class JobPostingController : Controller
 {
     private readonly IRespository<JobPosting> _repository;
@@ -18,18 +19,20 @@ public class JobPostingController : Controller
         _userManager = userManager;
         _repository = respository;
     }
+    [AllowAnonymous]
     public async Task<IActionResult> Index()
     {
         var jobPostings = await _repository.GetAllAsync();
         return View(jobPostings);
     }
-
+    [Authorize(Roles = "Admin,Employer")]
     public IActionResult Create()
     {
         return View();
     }
 
     [HttpPost]
+    [Authorize(Roles = "Admin,Employer")]
     public async Task<IActionResult> Create(JobPostingViewModel jobPostingVm)
     {
         if(ModelState.IsValid)
